ca7e5aca9003a03eebc84bdc00c3c57efeaed341
robertovers <oversr14@gmail.com>
2020-08-27 16:53:29 +1000

    Add method to automatically generate playlists in the user's library

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import requests
 import json
 
-endpoint_url = 'https://api.spotify.com/v1/recommendations?'
-access_token = "ACCESS_TOKEN"
+endpoint_url = "https://api.spotify.com/v1/recommendations?"
+access_token = "ACCESS TOKEN HERE"
 uris = []
+
 
 # SONG FILTERS
 limit = 10
 market = "AU"
 seed_genres = "rnb"
 target_danceability = 0.2
-seed_artists = '5dCvSnVduaFleCnyy98JMo'
+seed_artists = '7daSp9zXk1dmqNxwKFkL35'
+
 
 # QUERY
-query = f'{endpoint_url}limit={limit}&market={market}&seed_genres={seed_genres}&target_danceability={target_danceability}&seed_artists={seed_artists}'
+query = f'{endpoint_url}limit={limit}&market={market}&seed_genres={seed_genres} \
+        &target_danceability={target_danceability}&seed_artists={seed_artists}'
 
 response = requests.get(query,
     headers={"Content-Type":"application/json",
@@ -23,3 +26,29 @@ json_response = response.json()
 for i, j in enumerate(json_response['tracks']):
     uris.append(j['uri'])
     print(f"{i+1}) \"{j['name']}\" by {j['artists'][0]['name']}")
+
+
+# CREATE PLAYLIST
+user_id = "USER ID HERE"
+endpoint_url = f"https://api.spotify.com/v1/users/{user_id}/playlists"
+
+request_body = json.dumps({
+          "name": "Auto-Generated Playlist",
+          "description": "\ (•◡•) /",
+          "public": False
+        })
+
+response = requests.post(url=endpoint_url, data=request_body, headers={"Content-Type":"application/json", "Authorization":"Bearer " + access_token})
+print(response.status_code)
+
+
+# ADD TRACKS
+playlist_id = response.json()['id']
+endpoint_url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
+
+request_body = json.dumps({
+          "uris":uris
+        })
+
+response = requests.post(url=endpoint_url, data=request_body, headers={"Content-Type":"application/json", "Authorization":f"Bearer {access_token}"})
+print(response.status_code)
